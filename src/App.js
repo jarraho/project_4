a64e9ae4006ba150adb040568ec8d34fcc9c5ccb
obaida <aljarraho18@gmail.com>
2021-09-27 18:16:16 +0300

    add product page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { React } from "react";
+import React from "react";
+import Product from "./components/Product";
 import data from "./data";
-
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">
+          <a className="brand" href="/">
             amazona
           </a>
         </div>
@@ -19,38 +19,41 @@ function App() {
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt="product.name"
-                  />
-                </a>
-                <div className="card-body">
+              <>
+                <div key={product._id} className="card">
                   <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
+                    <img
+                      className="medium"
+                      src={product.image}
+                      alt={product.name}
+                    />
                   </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star-0"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star-half-o"></i>
-                    </span>
+                  <div className="card-body">
+                    <a href={`/product/${product._id}`}>
+                      <h2>{product.name}</h2>
+                    </a>
+                    <div className="rating">
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                      <span>
+                        <i className="fa fa-star"></i>
+                      </span>
+                    </div>
+                    <div className="price">${product.price}</div>
                   </div>
-                  <div className="price">${product.price}</div>
                 </div>
-              </div>
+                <Product key={product._id} product={product}></Product>
+              </>
             ))}
           </div>
         </div>
@@ -59,5 +62,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
